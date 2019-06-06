061b47588b0a9a4393daa9f64fd52e3ba78c270b
zhangshulong <441985745@qq.com>
2019-06-06 09:52:36 +0800

    注解中设置超时,重试次数,版本等信息

--- boot-order-service-consumer/src/main/java/com/atguigu/gmall/service/impl/OrderServiceImpl.java
+++ boot-order-service-consumer/src/main/java/com/atguigu/gmall/service/impl/OrderServiceImpl.java
@@ -11,7 +11,22 @@ import com.atguigu.gmall.service.OrderService;
 import com.atguigu.gmall.service.UserService;
 @Service
 public class OrderServiceImpl implements OrderService{
-	@Reference //帮我们远程引用服务
+	/**
+	 * 当然我们可以在consumer端通过@Reference注解的parameters注解实现（PS：基于XML的配置可以配置MethodConfig元素，
+	 * but基于注解的配置没有单独设置某个method的配置，只能曲线救国，
+	 * 我们只要清楚一点就是所有的配置都会作为url参数的一部分就OK了，
+	 * 所以我们可以通过配置注解的parameters参数来往url上附加额外参数来达到相同的目的）:
+	 *  @Reference(
+	 *
+	 *             version = "${demo.service.version}",
+	 *             application = "${dubbo.application.id}",
+	 *             registry = "${dubbo.registry.id}",
+	 *             parameters = {"sayHello.timeout", "3000", "sayHello2.timeout", "5000"}
+	 *
+	 *     )
+	 *
+	 */
+	@Reference(version = "2.0.0") //帮我们远程引用服务,参数可以设置版本,重试次数,超时时间等
 	UserService userService;
 	public List<UserAddress> initOrder(String userId) {
 		// TODO Auto-generated method stub
