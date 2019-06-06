443ee68f5b3cc08632123833b80c77d5cb3a97f4
zhangshulong <441985745@qq.com>
2019-06-06 10:44:12 +0800

    将本地存根stub文件放入公用接口项目中

--- boot-user-service-provider/src/main/java/com/atguigu/gmall/service/impl/UserServiceImpl2.java
+++ boot-user-service-provider/src/main/java/com/atguigu/gmall/service/impl/UserServiceImpl2.java
@@ -1,20 +1,20 @@
 package com.atguigu.gmall.service.impl;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.stereotype.Component;
-
 import com.alibaba.dubbo.config.annotation.Service;
 import com.atguigu.gmall.bean.UserAddress;
 import com.atguigu.gmall.service.UserService;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
 @Component
-@Service(version = "1.0.0") //注意,这个是dubbo的注解,不是spring的,暴露服务
-public class UserServiceImpl implements UserService {
+@Service(version = "2.0.0") //注意,这个是dubbo的注解,不是spring的,暴露服务
+public class UserServiceImpl2 implements UserService {
 
 	@Override
 	public List<UserAddress> getUserAddressList(String userId) {
-		System.out.println("UserServiceImpl.....old...");
+		System.out.println("UserServiceImpl.....new...");
 		// TODO Auto-generated method stub
 		UserAddress address1 = new UserAddress(1, "北京市昌平区宏福科技园综合楼3层", "1", "李老师", "010-56253825", "Y");
 		UserAddress address2 = new UserAddress(2, "深圳市宝安区西部硅谷大厦B座3层（深圳分校）", "1", "王老师", "010-56253825", "N");
